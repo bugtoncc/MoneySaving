8c2d72f44abe3c61e839aca8a6c011dcae133059
Chanchai Thaiyanon <chanchai@kkumail.com>
2020-06-08 21:32:54 +0700

    added models

---
+++ MoneySaving/Models/Catagory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneySaving.Models {
+    public class Catagory {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int PocketID { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
---
+++ MoneySaving/Models/Pocket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneySaving.Models {
+    public class Pocket {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
