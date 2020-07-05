0c41130e5b678d3280b026333bc4a5a191c46b0c
Chanchai Thaiyanon <chanchai@kkumail.com>
2020-07-06 00:46:16 +0700

    buid CashFlowType form

--- MoneySaving/Models/CashflowType.cs
+++ MoneySaving/Models/CashflowType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,29 @@ namespace MoneySaving.Models
     public class CashflowType
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "*")]
         public string Name { get; set; }
+
+        [Display(Name = "Active")]
         public bool StatusFlag { get; set; }
+
+        [Display(Name = "Last Update")]
+        //[DataType(DataType.DateTime)]
+        //[DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime LastUpdate { get; set; }
+
+        [Display(Name= "Update By")]
         public string UpdateBy { get; set; }
 
+
+        //--- reference key ---//
         public List<MCategory> MCategories { get; set; }
+
+        public CashflowType()
+        {
+            LastUpdate = DateTime.Now;
+            UpdateBy = "ADMIN";
+        }
     }
 }
